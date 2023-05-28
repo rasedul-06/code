c857fe7680d311eb3bc2df1df26dc1b97793db15
Rasedul Islam <128040157+rasedul-06@users.noreply.github.com>
2023-05-28 16:37:15 +0600

    upload

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *arr = new int[10];
+
+    for( int i=0; i<10; i++ ){
+        cin >> arr[i];
+    }
+    for( int i=0; i<10; i++ ){
+        cout << arr[i];
+    }
+    return 0;
+}
